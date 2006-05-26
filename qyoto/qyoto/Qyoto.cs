a6c4168b7050faabcfa72a942065941e44a7b5d4
Richard Dale <richard.j.dale@gmail.com>
2006-05-26 11:51:08 +0000

    * Added code from Arno Rehn to access obtain the return value of   a classes Emit() method as an interface and obtain the C++ signal   type signatures and set them up in HashTables with a top level   table with the classes as keys, containing sub tables with the   C++ signatures as keys which map onto the correponding C#   MethodInfo as a value
    CCMAIL: kde-bindings@kde.org
    
    
    svn path=/trunk/playground/bindings/kimono/; revision=544893

--- qyoto/qyoto/Qyoto.cs
+++ qyoto/qyoto/Qyoto.cs
@@ -1,14 +1,74 @@
 namespace Qt
 {
 	using System;
+	using System.Collections;
+	using System.Reflection;
 	using System.Runtime.InteropServices;
 	
 	public class Qyoto : System.Object
 	{
 		[DllImport("libqyoto", CharSet=CharSet.Ansi)]
 		public static extern void Init_qyoto();
+		
+		/// This Hashtable contains a list of classes with their Hashtables for slots. The class name is the key, the slot-hashtable the value.
+		public static Hashtable classes = new Hashtable();
+		
+		public static void GetSlotSignatures(Type t) {
+			
+			/// Remove the old object if it already exists
+			classes.Remove(t.Name);
+			
+			/// This Hashtable contains the slots of a class. The C++ type signature is the key, the appropriate C# MethodInfo the value.
+			Hashtable slots = new Hashtable();
+			
+			MethodInfo[] mis = t.GetMethods( BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic );
+			
+			foreach (MethodInfo mi in mis) {
+				object[] attributes = mi.GetCustomAttributes(typeof(Q_SLOT), false);
+				foreach (Q_SLOT attr in attributes) {
+					slots.Add(attr.Signature, mi);
+					break;
+				}
+			}
+			
+			classes.Add(t.Name, slots);
+		}
+		
+		public static string[] GetSignalSignatures(QObject o) {
+			Type iface;
+			try {
+				iface = GetSignalsInterface(o);
+			}
+			catch {
+				return null;
+			}
+			MethodInfo[] mis = iface.GetMethods();
+			
+			/// the interface has no signals...
+			if (mis.Length == 0)
+				return null;
+				
+			string[] signatures = new string[mis.Length];
+			int i = 0;
+			
+			foreach (MethodInfo mi in mis) {
+				object[] attributes = mi.GetCustomAttributes(typeof(Q_SIGNAL), false);
+				foreach (Q_SIGNAL attr in attributes) {
+					signatures[i] = attr.Signature;
+				}
+				i++;
+			}
+			
+			return signatures;
+		}
+		
+		public static Type GetSignalsInterface(QObject o) {
+			Type t = o.GetType();
+			MethodInfo mi = t.GetMethod("Emit", BindingFlags.Instance | BindingFlags.NonPublic);
+			return mi.ReturnType;
+		}
 	}
-
+	
 	[AttributeUsage( AttributeTargets.Class )]
 	class SmokeClass : Attribute
 	{
