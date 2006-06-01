9443a3bc41cef1b439c9340ce186d11299596ff8
Paolo Capriotti <paolo.capriotti@gmail.com>
2006-06-01 16:02:06 +0000

    * Added tutorial 7
    svn path=/trunk/playground/bindings/kimono/; revision=547293

---
+++ qyoto/examples/tutorial/t7/lcdrange.cs
@@ -0,0 +1,72 @@
+using Qt;
+
+class LCDRange : QWidget {
+	private QSlider slider;
+
+	public LCDRange(QWidget parent) : base(parent) {
+		QLCDNumber lcd = new QLCDNumber(2);
+		lcd.SetSegmentStyle(QLCDNumber.SegmentStyle.Filled);
+		
+		slider = new QSlider(Qt.Orientation.Horizontal);
+		slider.SetRange(0,99);
+		slider.SetValue(0);
+		
+		Connect(slider, SIGNAL("valueChanged(int)"),
+				lcd, SLOT("display(int)"));
+		Connect(slider, SIGNAL("valueChanged(int)"),
+				this, SIGNAL("valueChanged(int)"));
+				
+		QVBoxLayout layout = new QVBoxLayout();
+		layout.AddWidget(lcd);
+		layout.AddWidget(slider);
+		
+		SetLayout(layout);
+		
+	}
+	public int Value() {
+		return slider.Value();
+	}
+	
+	[Q_SLOT("void setValue(int)")]
+	public void SetValue(int value) {
+		slider.SetValue(value);
+	}
+	
+	[Q_SIGNAL("void valueChanged(int)")]
+	public void ValueChanged(int newValue) { }
+	
+	
+}
+
+class MyWidget : QWidget {
+	public MyWidget(QWidget parent) : base(parent) {
+		QPushButton quit = new QPushButton("Quit");
+		Connect(quit, SIGNAL("clicked()"), qApp, SLOT("quit()"));
+		
+		QGridLayout grid = new QGridLayout();
+		LCDRange previousRange = null;
+		
+        for (int row = 0; row < 3; ++row) {
+            for (int column = 0; column < 3; ++column) {
+                LCDRange lcdRange = new LCDRange();
+                grid.AddWidget(lcdRange, row, column);
+                if (previousRange != null)
+                    Connect(lcdRange, SIGNAL("valueChanged(int)"),
+                            previousRange, SLOT("setValue(int)"));
+                previousRange = lcdRange;
+            }
+        }
+        
+		QVBoxLayout layout = new QVBoxLayout();
+        layout.AddWidget(quit);
+        layout.AddLayout(grid);
+        SetLayout(layout);
+	}
+	
+	public static void Main(string[] args) {
+		new QApplication(args);
+		MyWidget main = new MyWidget((QWidget)null);
+		main.Show();
+		QApplication.Exec();
+	}
+}
