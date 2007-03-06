504d69aec87b2fc27ba35da7dc8f7c1cb3a7fef1
Arno Rehn <arno@arnorehn.de>
2007-03-06 14:26:23 +0000

    * GCHandles for QModelIndex'es are now stored in a Dictionary.   QAbstractItemModel.CreateIndex() looks up a GCHandle in the Dictionary   and if it exists it is passed to the C++ function. Child items of a tree model   can now be selected.
    CCMAIL: kde-bindings@kde.org
    
    
    svn path=/trunk/playground/bindings/kimono/; revision=640008

--- qyoto/core/QAbstractItemModelExtras.cs
+++ qyoto/core/QAbstractItemModelExtras.cs
@@ -1,15 +1,18 @@
 namespace Qyoto {
 
 	using System;
+	using System.Collections.Generic;
 	using System.Runtime.InteropServices;
 
 	public partial class QAbstractItemModel : QObject {
 		[DllImport("libqyoto", CharSet=CharSet.Ansi)]
 		public static extern IntPtr AbstractItemModelCreateIndex(IntPtr obj, int row, int column, IntPtr ptr);
 		
+		private static Dictionary<WeakReference, GCHandle> handleMap = new Dictionary<WeakReference, GCHandle>();
+		
 		protected QModelIndex CreateIndex(int row, int column, object ptr) {
 			IntPtr ret = AbstractItemModelCreateIndex((IntPtr) GCHandle.Alloc(this),
-									row, column, (IntPtr) GCHandle.Alloc(ptr));
+									row, column, (IntPtr) GetIndexHandle(ptr));
 			return (QModelIndex) ((GCHandle) ret).Target;
 		}
 		
@@ -17,6 +20,23 @@ namespace Qyoto {
 			IntPtr ret = AbstractItemModelCreateIndex((IntPtr) GCHandle.Alloc(this),
 									row, column, IntPtr.Zero);
 			return (QModelIndex) ((GCHandle) ret).Target;
+		}
+		
+		private GCHandle GetIndexHandle(object o) {
+			foreach (WeakReference weakRef in handleMap.Keys) {
+				if (weakRef.Target == o) {
+					// found
+					return handleMap[weakRef];
+				}
+				
+				if (!weakRef.IsAlive) {
+					handleMap.Remove(weakRef);
+				}
+			}
+			
+			GCHandle handle = GCHandle.Alloc(o);
+			handleMap.Add(new WeakReference(o), handle);
+			return handle;
 		}
 	}
 }
