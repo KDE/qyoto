13a8ce9c964664bcfb69358a86510b0dffeeda46
Helio Castro <helio@kde.org>
2008-06-04 19:43:03 +0000

    - Required by gcc later that qt 4.3.x
    svn path=/trunk/KDE/kdebindings/csharp/; revision=816770

--- qyoto/src/qyotosmokebinding.cpp
+++ qyoto/src/qyotosmokebinding.cpp
@@ -13,6 +13,7 @@
  *                                                                         *
  ***************************************************************************/
 
+#include <stdlib.h>
 #include "qyotosmokebinding.h"
 #include "qyoto.h"
 #include "virtualmethodcall.h"
