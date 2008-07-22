fb05f01cd41230e6c08b3454321c0cae4df04e2c
Richard Dale <richard.j.dale@gmail.com>
2008-07-22 16:46:16 +0000

    * Promote compile problem fix for missing headers from the trunk
    svn path=/branches/KDE/4.1/kdebindings/csharp/; revision=836624

--- akonadi/src/akonadihandlers.cpp
+++ akonadi/src/akonadihandlers.cpp
@@ -19,50 +19,13 @@
 #include <smokeqyoto.h>
 #include <marshall_macros.h>
 
+#include <QModelIndex>
+
 #include <akonadi/agentinstance.h>
-#include <akonadi/agentinstancecreatejob.h>
-#include <akonadi/agentinstancemodel.h>
-#include <akonadi/agentinstanceview.h>
-#include <akonadi/agentinstancewidget.h>
 #include <akonadi/agenttype.h>
-#include <akonadi/agenttypemodel.h>
-#include <akonadi/agenttypeview.h>
-#include <akonadi/agenttypewidget.h>
 #include <akonadi/attribute.h>
-#include <akonadi/attributefactory.h>
 #include <akonadi/collection.h>
-#include <akonadi/collectioncombobox.h>
-#include <akonadi/collectioncopyjob.h>
-#include <akonadi/collectioncreatejob.h>
-#include <akonadi/collectiondeletejob.h>
-#include <akonadi/collectiondisplayattribute.h>
-#include <akonadi/collectionfetchjob.h>
-#include <akonadi/collectionfilterproxymodel.h>
-#include <akonadi/collectionmodel.h>
-#include <akonadi/collectionmodifyjob.h>
-#include <akonadi/collectionpathresolver.h>
-#include <akonadi/collectionpropertiesdialog.h>
-#include <akonadi/collectionpropertiespage.h>
-#include <akonadi/collectionstatistics.h>
-#include <akonadi/collectionstatisticsdelegate.h>
-#include <akonadi/collectionstatisticsjob.h>
-#include <akonadi/collectionstatisticsmodel.h>
-#include <akonadi/collectionview.h>
 #include <akonadi/item.h>
-#include <akonadi/itemcopyjob.h>
-#include <akonadi/itemcreatejob.h>
-#include <akonadi/itemdeletejob.h>
-#include <akonadi/itemdetailsview.h>
-#include <akonadi/itemfetchjob.h>
-#include <akonadi/itemfetchscope.h>
-#include <akonadi/itemmodel.h>
-#include <akonadi/itemmodifyjob.h>
-#include <akonadi/itemmonitor.h>
-#include <akonadi/itemmovejob.h>
-#include <akonadi/itemserializerplugin.h>
-#include <akonadi/itemsync.h>
-#include <akonadi/itemview.h>
-#include <akonadi/kmime/messagethreadingattribute.h>
 
 DEF_LIST_MARSHALLER( AkonadiAttributeList, QList<Akonadi::Attribute*>, Akonadi::Attribute )
 DEF_VALUELIST_MARSHALLER( AkonadiAgentInstanceList, QList<Akonadi::AgentInstance>, Akonadi::AgentInstance )
