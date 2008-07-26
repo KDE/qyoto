8fd3a75048ea3c11905e794957c8be89d764bb33
Richard Dale <richard.j.dale@gmail.com>
2008-07-26 17:13:49 +0000

    * Added some KTextEditor marshallers
    svn path=/trunk/KDE/kdebindings/csharp/; revision=838087

--- ktexteditor/src/ktexteditorhandlers.cpp
+++ ktexteditor/src/ktexteditorhandlers.cpp
@@ -19,6 +19,25 @@
 #include <smokeqyoto.h>
 #include <marshall_macros.h>
 
+#include <ktexteditor/commandinterface.h>
+#include <ktexteditor/document.h>
+#include <ktexteditor/smartrange.h>
+#include <ktexteditor/view.h>
+
+DEF_LIST_MARSHALLER( KTextEditorCommandList, QList<KTextEditor::Command*>, KTextEditor::Command )
+DEF_LIST_MARSHALLER( KTextEditorDocumentList, QList<KTextEditor::Document*>, KTextEditor::Document )
+DEF_LIST_MARSHALLER( KTextEditorSmartRangeList, QList<KTextEditor::SmartRange*>, KTextEditor::SmartRange )
+DEF_LIST_MARSHALLER( KTextEditorSmartRangeNotifierList, QList<KTextEditor::SmartRangeNotifier*>, KTextEditor::SmartRangeNotifier )
+DEF_LIST_MARSHALLER( KTextEditorSmartRangeWatcherList, QList<KTextEditor::SmartRangeWatcher*>, KTextEditor::SmartRangeWatcher )
+DEF_LIST_MARSHALLER( KTextEditorViewList, QList<KTextEditor::View*>, KTextEditor::View )
+
 TypeHandler KTextEditor_handlers[] = {
+    { "QList<KTextEditor::Command*>", marshall_KTextEditorCommandList },
+    { "QList<KTextEditor::Document*>&", marshall_KTextEditorDocumentList },
+    { "QList<KTextEditor::SmartRange*>", marshall_KTextEditorSmartRangeList },
+    { "QList<KTextEditor::SmartRange*>&", marshall_KTextEditorSmartRangeList },
+    { "QList<KTextEditor::SmartRangeNotifier*>", marshall_KTextEditorSmartRangeNotifierList },
+    { "QList<KTextEditor::SmartRangeWatcher*>&", marshall_KTextEditorSmartRangeWatcherList },
+    { "QList<KTextEditor::View*>&", marshall_KTextEditorViewList },
     { 0, 0 }
 };
