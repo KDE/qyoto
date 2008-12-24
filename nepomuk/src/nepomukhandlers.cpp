4a934b1ea9fa4576f6d9edfb9618db7887255285
Arno Rehn <arno@arnorehn.de>
2008-12-24 17:17:39 +0000

    * Make the marshallers active again and add nepomuk and nepomukqueryclient   as libs to link to. This is the same in the ruby bindings.
    CCMAIL: kde-bindings@kde.org
    
    
    svn path=/trunk/KDE/kdebindings/csharp/; revision=901214

--- nepomuk/src/nepomukhandlers.cpp
+++ nepomuk/src/nepomukhandlers.cpp
@@ -17,28 +17,28 @@
 
 #include <qyoto.h>
 #include <smokeqyoto.h>
-// #include <marshall_macros.h>
+#include <marshall_macros.h>
 
-// #include <nepomuk/class.h>
-// #include <nepomuk/property.h>
-// #include <nepomuk/resource.h>
-// #include <nepomuk/tag.h>
-// #include <nepomuk/variant.h>
+#include <nepomuk/class.h>
+#include <nepomuk/property.h>
+#include <nepomuk/resource.h>
+#include <nepomuk/tag.h>
+#include <nepomuk/variant.h>
 
-// DEF_VALUELIST_MARSHALLER( NepomukResourceList, QList<Nepomuk::Resource>, Nepomuk::Resource )
-// DEF_VALUELIST_MARSHALLER( NepomukTagList, QList<Nepomuk::Tag>, Nepomuk::Tag )
-// DEF_VALUELIST_MARSHALLER( NepomukTypesClassList, QList<Nepomuk::Types::Class>, Nepomuk::Types::Class )
-// DEF_VALUELIST_MARSHALLER( NepomukTypesPropertyList, QList<Nepomuk::Types::Property>, Nepomuk::Types::Property )
+DEF_VALUELIST_MARSHALLER( NepomukResourceList, QList<Nepomuk::Resource>, Nepomuk::Resource )
+DEF_VALUELIST_MARSHALLER( NepomukTagList, QList<Nepomuk::Tag>, Nepomuk::Tag )
+DEF_VALUELIST_MARSHALLER( NepomukTypesClassList, QList<Nepomuk::Types::Class>, Nepomuk::Types::Class )
+DEF_VALUELIST_MARSHALLER( NepomukTypesPropertyList, QList<Nepomuk::Types::Property>, Nepomuk::Types::Property )
 
 TypeHandler Nepomuk_handlers[] = {
-//     { "QList<Nepomuk::Resource>", marshall_NepomukResourceList },
-//     { "QList<Nepomuk::Resource>&", marshall_NepomukResourceList },
-//     { "QList<Nepomuk::Tag>", marshall_NepomukTagList },
-//     { "QList<Nepomuk::Tag>&", marshall_NepomukTagList },
-//     { "QList<Nepomuk::Types::Class>", marshall_NepomukTypesClassList },
-//     { "QList<Nepomuk::Types::Class>&", marshall_NepomukTypesClassList },
-//     { "QList<Nepomuk::Types::Property>", marshall_NepomukTypesPropertyList },
-//     { "QList<Nepomuk::Types::Property>&", marshall_NepomukTypesPropertyList },
+    { "QList<Nepomuk::Resource>", marshall_NepomukResourceList },
+    { "QList<Nepomuk::Resource>&", marshall_NepomukResourceList },
+    { "QList<Nepomuk::Tag>", marshall_NepomukTagList },
+    { "QList<Nepomuk::Tag>&", marshall_NepomukTagList },
+    { "QList<Nepomuk::Types::Class>", marshall_NepomukTypesClassList },
+    { "QList<Nepomuk::Types::Class>&", marshall_NepomukTypesClassList },
+    { "QList<Nepomuk::Types::Property>", marshall_NepomukTypesPropertyList },
+    { "QList<Nepomuk::Types::Property>&", marshall_NepomukTypesPropertyList },
 //    { "QHash<QUrl,Nepomuk::Variant>", marshall_QHashQUrlNepomukVariant },
     { 0, 0 }
 };
