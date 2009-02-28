05539e317a2b362ca1d960629b4be75ab58b1aad
Arno Rehn <arno@arnorehn.de>
2009-02-28 19:46:58 +0000

    bump qyoto version comment++
    svn path=/trunk/KDE/kdebindings/csharp/; revision=933377

--- qyoto/src/AssemblyInfo.cs
+++ qyoto/src/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Version number is the same as the version of the latest Qt version
-// for which the Qyoto runtime has been programmed.
-[assembly: AssemblyVersion("4.4.0.0")]
+// Version number is the same as the version of the latest Qt 
+// for which the Qyoto classes has been generated and the runtime has
+// been built
+[assembly: AssemblyVersion("4.5.0.0")]
 
 // Unnecessary, as the keyfile is given to the compiler as a parameter.
 // It may be useful to have it here, though.
