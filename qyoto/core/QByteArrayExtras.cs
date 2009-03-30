4d3328d4ad2c8d8a2d6337ac3acb37cd0d033f2a
Arno Rehn <arno@arnorehn.de>
2009-03-30 16:50:57 +0000

    implement a QByteArray ctor that takes a byte[] and a QByteArray.ToArray() method which returns a byte[].
    CCMAIL: eric@extremeboredom.net
    
    
    svn path=/trunk/KDE/kdebindings/csharp/; revision=946979

--- qyoto/core/QByteArrayExtras.cs
+++ qyoto/core/QByteArrayExtras.cs
@@ -1,6 +1,22 @@
 namespace Qyoto {
 	using System;
 	public partial class QByteArray : Object, IDisposable {
+		public QByteArray(byte[] array) : this(array.Length, '\0') {
+			Pointer<sbyte> p = Data();
+			for (int i = 0; i < array.Length; i++) {
+				p[i] = (sbyte) array[i];
+			}
+		}
+		
+		public byte[] ToArray() {
+			Pointer<sbyte> p = Data();
+			byte[] array = new byte[Size()];
+			for (int i = 0; i < Size(); i++) {
+				array[i] = (byte) p[i];
+			}
+			return array;
+		}
+		
 		public static implicit operator QByteArray(string arg) {
 			return new QByteArray(arg);
 		}
