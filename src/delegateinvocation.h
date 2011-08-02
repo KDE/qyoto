fca038e2f07271b1ef6c0f871f3b38e74b8cc4ba
Arno Rehn <arno@arnorehn.de>
2011-08-02 16:49:06 +0200

    fix copyright information. also backported to 4.7.
    CCMAIL: kde-bindings@kde.org

--- src/delegateinvocation.h
+++ src/delegateinvocation.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies), Contact: qt-info@nokia.com
+ * Copyright (C) 2008 Arno Rehn <arno@arnorehn.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ */
+
 #ifndef DELEGATEINVOCATION_H
 #define DELEGATEINVOCATION_H
 
